4a6387795c81f6482e99170094a5fe8e4a1f462f
Autumn Johnson <autumn@iris.washington.edu>
2022-04-26 16:18:02 -0700

    Additional whitespace fix caught by Python linter

--- backend/yasmine/app/settings.py
+++ backend/yasmine/app/settings.py
@@ -37,7 +37,7 @@ import os
 from yasmine.app.utils.op_sys import is_windows
 
 ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
-APP_DIR = os.path.abspath(os.path.join(__file__ , '../../../../data'))
+APP_DIR = os.path.abspath(os.path.join(__file__, '../../../../data'))
 
 RUN_ROOT = os.path.join(APP_DIR, '_run')
 
